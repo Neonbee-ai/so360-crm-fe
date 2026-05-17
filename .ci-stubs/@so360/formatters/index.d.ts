58635d914e570cebf2863f1ff2734edfbad1fbca
Praveen VR <praveen.vr@moonhive.in>
2026-05-17 11:41:10 +0400

    fix(ci): add missing exports to all @so360 CI stubs (useActivity, useBusinessSettings, Button, useFormatters)

--- .ci-stubs/@so360/formatters/index.d.ts
+++ .ci-stubs/@so360/formatters/index.d.ts
@@ -1,5 +1,5 @@
-export const useShellBridge: any;
-export const useShell: any;
-export const eventBus: any;
+export declare const useFormatters: () => { formatCurrency: (v: any) => string; formatDate: (v: any) => string; formatNumber: (v: any) => string };
+export declare const formatCurrency: (v: any) => string;
+export declare const formatDate: (v: any) => string;
 declare const _: any;
 export default _;
--- .ci-stubs/@so360/shell-context/index.js
+++ .ci-stubs/@so360/shell-context/index.js
@@ -1,4 +1,16 @@
 export const useShellBridge = () => null;
-export const useShell = () => ({});
+export const useShell = () => ({
+  isModuleEnabled: () => false,
+  isFeatureHidden: () => false,
+  isFeatureEnabled: () => true,
+});
+export const useBusinessSettings = () => ({ settings: { base_currency: 'USD', timezone: 'UTC' } });
+export const useNotify = () => ({ emitNotification: () => {} });
+export const useActivity = () => ({ recordActivity: async () => {} });
+export const usePeople = () => ({ people: [] });
+export const useTenant = () => ({ id: 'test-tenant' });
+export const useOrganization = () => ({ id: 'test-org' });
+export const useIdentity = () => ({ user: null });
+export const ShellContext = {};
 export const eventBus = { publish: () => {}, subscribe: () => () => {} };
 export default {};
